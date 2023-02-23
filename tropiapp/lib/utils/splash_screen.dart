02a52d09ee393caf0f635473556d10e15335eb25
senoadhi <senoadhin@gmail.com>
2023-02-23 23:34:04 +0700

    Splash Screen

--- tropiapp/lib/utils/splash_screen.dart
+++ tropiapp/lib/utils/splash_screen.dart
@@ -1,17 +1,18 @@
 import 'package:flutter/material.dart';
-import 'package:lottie/lottie.dart';
-import 'package:get/get.dart';
+import 'package:flutter_svg/svg.dart';
 
 class SplashScreen extends StatelessWidget {
+  const SplashScreen({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        body: Center(
-          child: Container(
-            width: Get.width * 0.75,
-            height: Get.width * 0.75,
-            child: Lottie.asset("assets/lottie/splash-panacea.json"),
+        body: Container(
+          alignment: Alignment.center,
+          color: Colors.orange,
+          child: Center(
+            child: SvgPicture.asset('assets/icons/logo3.svg'),
           ),
         ),
       ),
